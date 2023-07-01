eff25f7ed3cf40cc90205bb09c99ae9720e8baf8
rada-i <neposebanmail@gmail.com>
2023-07-01 15:05:51 +0200

    corrections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,29 @@
 import React from "react";
-import { data } from "./data/data";
-// import cv from "./cv/cv.pdf";
 import Image from "next/image";
+import { data } from "./data/data";
 
 const AboutMe: React.FC = () => {
+  // Define the type for the 'data' object
+  type Data = {
+    title: string;
+    body: string;
+  };
+
+  // Replace the 'data' object with the typed version
+  const aboutData: Data = {
+    title: data.title,
+    body: data.body,
+  };
   return (
     <div className="container h-screen ">
-      <div className=" textContainer  lg:sp sm:text-[8px] sm:leading-none sm:opacity-100 md:text-[10px] md:leading-none lg:text-[13px] xl:text-2xl 2xl:text-3xl">
+      <div className="textContainer lg:sp sm:text-[8px] sm:leading-none sm:opacity-100 md:text-[10px] md:leading-none lg:text-[13px] xl:text-2xl 2xl:text-3xl">
         <div className="text opacity-50">
-          <h2>{data.title}</h2>
-          <p className=" leading-loose">{data.body}</p>
+          <h2>{aboutData.title}</h2>
+          <p className="leading-loose">{aboutData.body}</p>
         </div>
         {/* <a href={cv} download="your_cv.pdf">
           <button
-            className="min-w-100 font-display  rounded-full   bg-indigo-400 px-6 py-2 font-bold   text-black opacity-100 hover:bg-blue-700
+            className="min-w-100 font-display rounded-full bg-indigo-400 px-6 py-2 font-bold text-black opacity-100 hover:bg-blue-700
               hover:text-white sm:bg-none"
           >
             Download CV
@@ -28,7 +38,7 @@ const AboutMe: React.FC = () => {
           aria-label={"My image"}
           alt={"My image"}
           src={"/images/cvvv.jpg"}
-          className="h-250 w-500  img img  rounded-3xl object-cover opacity-100 "
+          className="h-250 w-500 img img rounded-3xl object-cover opacity-100 "
         />
       </div>
     </div>
